b16415cd232cc58bcdafe0c28dd8a222f19dfd4e
Nazmul <technazmul.hoque@gmail.com>
2026-07-31 12:51:19 -0400

    Defined responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button>Click Me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem hideBelow="lg" area="aside" bg="blue">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="yellow">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
