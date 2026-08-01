8163f3f94b94b61443a3ee3d688094e5e2f04194
Nazmul <technazmul.hoque@gmail.com>
2026-08-01 11:29:16 -0400

    Developed generic custom hook for fetching data and feteched genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
         <Navbar></Navbar>
       </GridItem>
       <GridItem hideBelow="lg" area="aside">
-        Aside
+        <GenreList />
       </GridItem>
       <GridItem area="main">
         <GameGrid />
